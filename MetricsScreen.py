74e33b80d9b6a5fddbd6d7046fad113f4046decc
Wojtek <wojciech.chanas@iu-study.org>
2023-03-04 01:58:27 +0100

    minor changes in string variables

--- MetricsScreen.py
+++ MetricsScreen.py
@@ -21,9 +21,22 @@ class FrameBoxLayout(MDBoxLayout):
         self.rect.size = instance.size
 
 
+def calculate_avg_progress():
+    from functions import habits
+    try:
+        progress_lst = [len(habit.history)/int(habit.goal) for habit in habits]
+        avg_progress = sum(progress_lst)/len(progress_lst)
+        print("list: ", progress_lst)
+        print("avg_p:", avg_progress)
+    except ZeroDivisionError:
+        avg_progress = 0
+    return avg_progress
+
+
 class MetricsScreen(MDScreen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.oldest_habit = None
         self.label = None
         self.longest_weekly = None
         self.longest_daily = None
@@ -31,7 +44,7 @@ class MetricsScreen(MDScreen):
         self.avg_progress_bar = None
         self.longest_all = None
         self.longest_monthly = None
-        back_button = Button(text='Back to menu', size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.2})
+        back_button = Button(text='Back to menu', size_hint=(0.2, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.1})
         back_button.bind(on_release=self.back)
         self.add_widget(back_button)
 
@@ -50,7 +63,7 @@ class MetricsScreen(MDScreen):
                             f"{longest_streak_all[2].lower()}."
         except ValueError:
             lgst_strk_all = "No habit match the search criteria."
-        self.longest_all = MDLabel(text=f'The habit with the longest streak of all habits is \n{lgst_strk_all}',
+        self.longest_all = MDLabel(text=f'The habit with the longest streak of all habits is: \n{lgst_strk_all}',
                                    font_size=24, size_hint=(0.9, 0.9), height=50,
                                    pos_hint={'center_x': 0.5, 'center_y': 0.5})
 
@@ -60,7 +73,7 @@ class MetricsScreen(MDScreen):
             most_freq_done_str = f"{most_freq_done[1]} which has been done {most_freq_done[0]} time(s)"
         except ValueError:
             most_freq_done_str = "No habit match the search criteria."
-        self.most_freq_done = MDLabel(text=f'The most frequently done habit is \n{most_freq_done_str}',
+        self.most_freq_done = MDLabel(text=f'The most frequently done habit is: \n{most_freq_done_str}',
                                       font_size=24, size_hint=(0.9, 0.9), height=50,
                                       pos_hint={'center_x': 0.5, 'center_y': 0.5})
 
@@ -68,11 +81,11 @@ class MetricsScreen(MDScreen):
         try:
             longest_streak_daily = max([(habit.longest_streak, habit.name) for habit in habits if habit.frequency ==
                                         "Days"], key=lambda x: x[0])
-            lgst_strk_daily = f"{longest_streak_daily[1]}, with a streak of {longest_streak_daily[0]} day(s)."
+            lgst_strk_daily = f"{longest_streak_daily[1]}, with the longest streak of {longest_streak_daily[0]} day(s)."
         except ValueError:
             lgst_strk_daily = "No habit match the search criteria."
 
-        self.longest_daily = MDLabel(text=f'The longest streak among daily habits is \n{lgst_strk_daily}',
+        self.longest_daily = MDLabel(text=f'The longest streak among daily habits belongs to: \n{lgst_strk_daily}',
                                      font_size=24, size_hint=(0.9, 0.9), height=50,
                                      pos_hint={'center_x': 0.5, 'center_y': 0.5})
 
@@ -80,30 +93,42 @@ class MetricsScreen(MDScreen):
         try:
             longest_streak_week = max([(habit.longest_streak, habit.name) for habit in habits if habit.frequency ==
                                        "Weeks"], key=lambda x: x[0])
-            lgst_strk_weekly = f'{longest_streak_week[1]}, with a streak of {longest_streak_week[0]} week(s).'
+            lgst_strk_weekly = f'{longest_streak_week[1]}, with the longest streak of {longest_streak_week[0]} week(s).'
         except ValueError:
             lgst_strk_weekly = "No habit match the search criteria."
-        self.longest_weekly = MDLabel(text=f'The longest streak among weekly habits is \n{lgst_strk_weekly}',
+        self.longest_weekly = MDLabel(text=f'The longest streak among weekly belongs to: \n{lgst_strk_weekly}',
                                       font_size=24, size_hint=(0.9, 0.9), height=50,
                                       pos_hint={'center_x': 0.5, 'center_y': 0.5})
 
         # The longest streak of monthly habits
         try:
-            longest_streak_month = str(max([habit.longest_streak for habit in habits if habit.frequency == "Months"])) \
-                                   + "month(s)"
+            longest_streak_month = max([(habit.longest_streak, habit.name) for habit in habits if habit.frequency ==
+                                        "Months"], key=lambda x: x[0])
         except ValueError:
             longest_streak_month = "No habit match the search criteria."
-        self.longest_monthly = MDLabel(text=f"The longest streak among monthly habits: \n{longest_streak_month}",
+        self.longest_monthly = MDLabel(text=f"The longest streak among monthly habits belongs to: "
+                                            f"\n{longest_streak_month[1]}"
+                                            f" with the longest streak of {longest_streak_month[0]} month(s)",
                                        font_size=24, size_hint=(0.9, 0.9), height=50,
                                        pos_hint={'center_x': 0.5, 'center_y': 0.5})
 
+        # The oldest habit
+        try:
+            oldest_habit = sorted([(habit.history, habit.name)for habit in habits], key=lambda x: x[0])
+            oldest_habit_str =f"The oldest habit is: {oldest_habit[0][1]}, it was first done on {oldest_habit[0][0][0]}"
+        except IndexError:
+            oldest_habit_str = "The oldest habit is: No habit has been done yet!"
+        self.oldest_habit = MDLabel(text=oldest_habit_str,
+                                    font_size=24, size_hint=(0.9, 0.9), height=50,
+                                    pos_hint={'center_x': 0.5, 'center_y': 0.5})
+
         # Average progress bar
-        avg_progress = self.calculate_avg_progress()
+        avg_progress = calculate_avg_progress()
         print(avg_progress)
         self.label = MDLabel(text=f'On average you\'ve reached {round(100*avg_progress, 2)}% of your goal! Keep it up!',
                              pos_hint={'center_x': 0.5, 'center_y': 0.5}, size_hint=(0.9, 0.4))
         self.avg_progress_bar = MDProgressBar(value=int(100*avg_progress), pos_hint={'center_x': 0.5, 'center_y': 0.5},
-                                              size_hint=(0.9, 0.2))
+                                              size_hint=(0.95, 0.02))
 
         # Create FrameBoxLayouts
         longest_daily_box = FrameBoxLayout(orientation='vertical', size_hint=(0.9, 0.1), pos_hint={'center_x': 0.5,
@@ -117,7 +142,9 @@ class MetricsScreen(MDScreen):
         most_freq_done_box = FrameBoxLayout(orientation='vertical', size_hint=(0.9, 0.1), pos_hint={'center_x': 0.5,
                                                                                                     'center_y': 0.46})
         average_progress_box = FrameBoxLayout(orientation='vertical', size_hint=(0.9, 0.1), pos_hint={'center_x': 0.5,
-                                                                                                      'center_y': 0.35})
+                                                                                                      'center_y': 0.24})
+        oldest_habit_box = FrameBoxLayout(orientation='vertical', size_hint=(0.9, 0.1), pos_hint={'center_x': 0.5,
+                                                                                                  'center_y': 0.35})
 
         # Add widgets to FrameBoxLayouts
         longest_daily_box.add_widget(self.longest_daily)
@@ -127,6 +154,7 @@ class MetricsScreen(MDScreen):
         most_freq_done_box.add_widget(self.most_freq_done)
         average_progress_box.add_widget(self.label)
         average_progress_box.add_widget(self.avg_progress_bar)
+        oldest_habit_box.add_widget(self.oldest_habit)
 
         # Add FrameBoxLayouts to the Screen
         self.add_widget(most_freq_done_box)
@@ -135,18 +163,4 @@ class MetricsScreen(MDScreen):
         self.add_widget(longest_monthly_box)
         self.add_widget(longest_all_box)
         self.add_widget(average_progress_box)
-
-    def calculate_avg_progress(self):
-        from functions import habits
-        try:
-            progress_lst = [len(habit.history)/int(habit.goal) for habit in habits]
-            avg_progress = sum(progress_lst)/len(progress_lst)
-            print("list: ", progress_lst)
-            print("avg_p:", avg_progress)
-        except ZeroDivisionError:
-            avg_progress = 0
-        return avg_progress
-
-    # def on_leave(self, *args):
-    #     """ Clears the text fields """
-    #     self.clear_widgets()
+        self.add_widget(oldest_habit_box)
