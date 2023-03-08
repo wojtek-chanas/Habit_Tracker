fc7afeabc19ae0378f9c43621e9ce7bd74c97b55
Wojtek <wojciech.chanas@iu-study.org>
2023-03-08 18:59:21 +0100

    bugs fixed

--- habit.py
+++ habit.py
@@ -17,82 +17,75 @@ class Habit:
         self.creation_date = None
         self.streak_str = None
 
-    def count_streak(self):
-        """ Returns current day-streak. """
+    def count_streak(self) -> str:
+        """ Computes streak value (int), assigns it to streak attribute and returns current streak (str). """
         sorted_history = sorted(self.history, reverse=False)
-        # Check if self.history isn't empty
-        if len(self.history) == 0:
-            return '-'
-        elif self.frequency == 'Days':
-            self.streak = 0
-            for i in range(0, len(self.history)):
-                if sorted_history[i] - sorted_history[i - 1] <= timedelta(days=1):
-                    self.streak += 1
 
-                    # update the longest streak if current streak is longer
-                    if self.streak >= self.longest_streak:
-                        self.longest_streak = self.streak
+        if self.frequency == 'Days':
+            # Check if self.history isn't empty
+            if len(self.history) == 0:
+                self.streak = 0
+            elif len(self.history) == 1:
+                self.streak = 1
+            else:
+                self.streak = 0
+                for i in range(0, len(self.history)):
+                    if sorted_history[i] - sorted_history[i - 1] <= timedelta(days=1):
+                        self.streak += 1
+                        # update the longest streak if current streak is longer
+                        if self.streak >= self.longest_streak:
+                            self.longest_streak = self.streak
 
-                elif sorted_history[i] - sorted_history[i - 1] > timedelta(days=1):  # break the streak
-                    self.streak = 1
-
-                elif len(self.history) == 1:
-                    self.streak = 1
+                    elif sorted_history[i] - sorted_history[i - 1] > timedelta(days=1):  # break the streak
+                        self.streak = 1
 
             outcome = f'{self.streak} day(s)'
             return outcome
 
         elif self.frequency == 'Weeks':
-            self.streak = 0
-            if len(self.history) == 1:
+            if len(self.history) == 0:
+                self.streak = 0
+            elif len(self.history) == 1:
                 self.streak = 1
-            for i in range(0, len(self.history)):
-                # datetime.date.isocalendar() allows to compare week numbers, so the amount of days in between each
-                # completion of the habit doesn't matter, as long as it's marked completed in two consecutive weeks
-                if sorted_history[i].isocalendar().week - sorted_history[i - 1].isocalendar().week == 1:
-                    self.streak += 1
-                    # update the longest streak if current streak is longer
-                    if self.streak >= self.longest_streak:
-                        self.longest_streak = self.streak
+            else:
+                self.streak = 1
+                for i in range(0, len(self.history)):
+                    # datetime.date.isocalendar() allows to compare week numbers, so the amount of days in between each
+                    # completion of the habit doesn't matter, as long as it's marked completed in two consecutive weeks
+                    if sorted_history[i].isocalendar().week - sorted_history[i - 1].isocalendar().week == 1:
+                        self.streak += 1
+                        # update the longest streak if current streak is longer
+                        if self.streak >= self.longest_streak:
+                            self.longest_streak = self.streak
 
-                elif sorted_history[i].isocalendar().week - sorted_history[i - 1].isocalendar().week > 1:
-                    # Break the streak
-                    self.streak = 1
+                    elif sorted_history[i].isocalendar().week - sorted_history[i - 1].isocalendar().week > 1:
+                        # Break the streak
+                        self.streak = 1
 
-                elif len(self.history) == 1:
-                    self.streak = 1
-                else:
-                    pass
             outcome = f'{self.streak} week(s)'
             return outcome
 
         elif self.frequency == 'Months':
-            self.streak = 0
-            if len(self.history) == 1:
+            if len(self.history) == 0:
+                self.streak = 0
+            elif len(self.history) == 1:
                 self.streak = 1
-            for i in range(0, len(self.history)):
-                # compare month numbers and add extra condition for habit done consecutively in december and january
-                if sorted_history[i].month - sorted_history[i - 1].month == 1 or \
-                        (sorted_history[i].month == 1 and sorted_history[i - 1].month == 12) and sorted_history[i].year\
-                        - sorted_history[i-1].year == 1:
-                    self.streak += 1
-                    # update the longest streak if current streak is longer
-                    if self.streak >= self.longest_streak:
-                        self.longest_streak = self.streak
+            else:
+                self.streak = 1
+                for i in range(0, len(self.history)):
+                    # compare month numbers and add extra condition for habit done consecutively in december and january
+                    if sorted_history[i].month - sorted_history[i - 1].month == 1 \
+                            or (sorted_history[i].month == 1 and sorted_history[i - 1].month == 12) \
+                            and sorted_history[i].year - sorted_history[i - 1].year == 1:
+                        self.streak += 1
+                        # update the longest streak if current streak is longer
+                        if self.streak >= self.longest_streak:
+                            self.longest_streak = self.streak
 
-                # Break the streak
-                elif sorted_history[i].month - sorted_history[i - 1].month > 1:
-                    self.streak = 1
+                    # Break the streak
+                    elif sorted_history[i].month - sorted_history[i - 1].month > 1:
+                        self.streak = 1
 
-                # don't change anything if habit was done multiple times in the same month
-                elif sorted_history[i].month == sorted_history[i - 1].month:
-                    pass
-
-                # set streak to 1 once the habit is completed for the first time
-                elif len(self.history) == 1:
-                    self.streak = 1
-                else:
-                    pass
             outcome = f'{self.streak} month(s)'
             return outcome
 
@@ -112,13 +105,13 @@ class Habit:
 
     def count_progress(self) -> str:
         """ Assigns and returns self.progress attribute.  """
-        counter = 0
+        counter = 1
         try:
             sorted_history = sorted(self.history, reverse=False)
             if self.frequency == 'Days':
+                counter = 0
                 for i in range(0, len(self.history)):
-                    if sorted_history[i] - sorted_history[i - 1] <= timedelta(days=1):
-                        counter += 1
+                    counter += 1
             elif self.frequency == 'Weeks':
                 for i in range(0, len(self.history)):
                     # datetime.date.isocalendar() allows to compare week numbers, so the amount of days in between each
@@ -128,9 +121,9 @@ class Habit:
             elif self.frequency == 'Months':
                 for i in range(0, len(self.history)):
                     # compare month numbers and add extra condition for habit done consecutively in december and january
-                    if sorted_history[i].month - sorted_history[i - 1].month == 1 or \
-                            (sorted_history[i].month == 1 and sorted_history[i - 1].month == 12) and sorted_history[i].year\
-                            - sorted_history[i-1].year == 1:
+                    if sorted_history[i].month - sorted_history[i - 1].month == 1 \
+                            or (sorted_history[i].month == 1 and sorted_history[i - 1].month == 12) \
+                            and sorted_history[i].year - sorted_history[i-1].year == 1:
                         counter += 1
         except IndexError:
             if len(self.history) == 0:
